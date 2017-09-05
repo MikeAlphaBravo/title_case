f3dfa82f613a3b683d916da57bbd756a08666e06
Michael Brooks and Sara Spink <sara@>
2017-09-05 16:11:18 -0700

    add working specs and test for title case program.

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,33 +1,8 @@
-# array = []
-# input
-# x = 0
-# def ping_pong do |input|
-#   count_to = ( x += 1 )
-#    >= input
-# end
-
-# this is what we want to be able to type into IRB to run ping_pong
-# ping_pong(15)
-
-
-
-puts "Please enter a number: "
-input = gets.chomp
-
-def make_array
-x = 0
-array = []
-  while (x < input)
-    x += 1
-    array.push(x)
+#!/usr/bin/env ruby
+def title_case(title)
+  split_title = title.split
+  split_title.each do |subject|
+    subject.capitalize!()
   end
-  array
-end
-
-
-
-if
-  this number divisable by 3 = ping
-else
-  this number divisable by 5 = pong
+  split_title.join(" ")
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -1,8 +1,12 @@
 require ('rspec')
-require ('ping_pong')
+require ('title_case')
 
-describe('#ping_pong') do
-  it("capitalizes the first letter of a word") do
-    expect(("beowulf").ping_pong()).to(eq("Beowulf"))
-  end
+describe('#title_case') do
+ it("capitalizes the first letter of a word") do
+   expect(title_case("title")).to(eq("Title"))
+ end
+
+ it("capitalizes the first letter of every word in a string") do
+   expect(title_case("this title of a book")).to(eq("This Title Of A Book"))
+ end
 end
